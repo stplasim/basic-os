af2f0941a72b7bb8c0e22bbc2a543461f82821f7
Simon Planinschek <planinscheksimon@outlook.com>
2020-09-20 12:07:17 +0200

    Add mail boot-loader function assembly file

---
+++ boot_sector_fuctions/boot_sector_main.asm
@@ -0,0 +1,33 @@
+[org 0x7c00] ; tell the assembler that our offset is bootsector code
+
+; The main routine makes sure the parameters are ready and then calls the function
+mov bx, HELLO
+call print
+call print_nl
+
+mov bx, BYE
+call print
+call print_nl
+
+mov dx, 0x12fe
+call print_hex
+
+; that's it! we can hang now
+jmp $
+
+
+; remember to include subroutines below the hang
+%include "boot_sector_print.asm"
+%include "boot_sector_print_hex.asm"
+
+; data
+HELLO:
+    db 'Hello world', 0
+
+BYE:
+    db 'See you later', 0
+
+
+; padding and magic number
+times 510-($-$$) db 0
+dw 0xaa55
