5826c57e0cab3b5adf61fffb173701abadb5262f
Simon Planinschek <planinscheksimon@outlook.com>
2020-09-21 12:30:45 +0200

    Add screen driver

---
+++ os/driver/screen.h
@@ -0,0 +1,28 @@
+#ifndef OS_SCREEN_H
+#define OS_SCREEN_H
+
+#define VIDEO_ADDRESS 0xb8000
+#define MAX_ROWS 25
+#define MAX_COLS 80
+#define WHITE_ON_BLACK 0x0f
+#define RED_ON_WHITE 0xf4
+
+/* Screen i/o ports */
+#define REG_SCREEN_CTRL 0x3d4
+#define REG_SCREEN_DATA 0x3d5
+
+/* Public kernel API */
+void clear_screen();
+void bos_print_at(char* message, int col, int row);
+void bos_print(char* message);
+void bos_print_backspace();
+
+/* Declaration of private functions */
+int get_cursor_offset();
+void set_cursor_offset(int offset);
+int print_char(char c, int col, int row, char attr);
+int get_offset(int col, int row);
+int get_offset_row(int offset);
+int get_offset_col(int offset);
+
+#endif //OS_SCREEN_H
