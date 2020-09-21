30bc3ca9794d66b17fac01ced87bf47b639b25d9
Simon Planinschek <planinscheksimon@outlook.com>
2020-09-21 12:27:07 +0200

    add address types

---
+++ os/cpu/type.h
@@ -0,0 +1,9 @@
+#ifndef OS_TYPE_H
+#define OS_TYPE_H
+
+#include <stdint.h>
+
+#define low_16(address) (uint16_t)((address) & 0xFFFF)
+#define high_16(address) (uint16_t)(((address) >> 16) & 0xFFFF)
+
+#endif //OS_TYPE_H
